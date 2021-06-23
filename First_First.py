65aa8b12fb1b5b5a7d6e41cf1671cf41c981284a
Curtis <=>
2021-06-22 23:36:02 -0400

    Fix argument errors

--- First_First.py
+++ First_First.py
@@ -1,4 +1,4 @@
 # StartExample, "open the door"
 
-def branch(zero, a, b, c):
-	print(zero, a, b, c) # Prints all three optional arguments, although you can have anywhere from zero to infinity
+def branch(name, iq, arg1, arg2, arg3):
+	print(name, iq, arg1, arg2, arg3) # Prints all optional arguments, although you can have anywhere from zero to infinity
